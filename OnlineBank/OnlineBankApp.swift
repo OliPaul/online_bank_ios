6f1eb152d1334eedc288e66de1e4d20dcf7172d9
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-06-11 10:56:15 +0200

    Initial Commit

---
+++ OnlineBank/OnlineBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineBankApp.swift
+//  OnlineBank
+//
+//  Created by Paul Olivier on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnlineBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
